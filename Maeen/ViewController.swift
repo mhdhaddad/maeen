5fbcd41b4f1e85576573e01829724d9671255613
yahya.alshaar <yahya.alshaar@wb-technologoies.com>
2018-05-23 01:23:52 +0300

    Initial Commit

---
+++ Maeen/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Maeen
+//
+//  Created by yahya alshaar on 5/23/18.
+//  Copyright © 2018 yahya alshaar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
